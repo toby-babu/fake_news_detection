aac874214c32984059ed9d62032ffd618aee5725
Toby Babu <tobybabu@Tobys-MacBook-Pro.local>
2018-04-29 16:39:24 -0400

    Initial model code

--- JsonParser/Config.py
+++ JsonParser/Config.py
@@ -3,12 +3,26 @@ ROOT = "ROOT"
 NULL = "NULL"
 NONEXIST = -1
 
-max_iter = 1001
-batch_size = 10000
 hidden_size = 200
-embedding_size = 50
 learning_rate = 0.1
-display_step = 100
 validation_step = 200
-n_Tokens = 48
-lam = 1e-8
+
+
+embedding_dim = 50
+filter_sizes=[3,4,5]
+num_filters=128
+dropout_keep_prob=0.5
+l2_reg_lambda=0.0
+
+# Training parameters
+batch_size=16
+num_epochs=500
+evaluate_every=100
+stop_step=1500
+display_step = 10
+num_classes = 2
+no_intervals_event = 20
+words_per_interval = 50
+# Misc Parameters
+allow_soft_placement=True
+log_device_placement=False
--- JsonParser/parsetweet.py
+++ JsonParser/parsetweet.py
@@ -1,3 +1,7 @@
+import datetime
+
+import nltk
+from nltk.corpus import stopwords
 import collections
 import json
 from pprint import pprint
@@ -6,13 +10,92 @@ import calendar
 import Config
 import pickle
 import numpy as np
-import nltk
-import string
-from nltk.corpus import stopwords
+
+import tensorflow as tf
+import numpy as np
+import Config
 
 nltk.download('punkt')
 nltk.download('stopwords')
 stop_words = set(stopwords.words('english'))
+
+class TextCNN(object):
+    """
+    A CNN for text classification.
+    Uses an embedding layer, followed by a convolutional, max-pooling and softmax layer.
+    """
+    def __init__(self,  embedding_array):
+        # Placeholders for input, output and dropout
+        self.embeddings = tf.Variable(embedding_array, dtype=tf.float32)
+        self.input_x = tf.sparse_placeholder(tf.int32, [None, Config.no_intervals_event, None], name="input_x")
+        self.input_y = tf.placeholder(tf.float32, [None, Config.num_classes], name="input_y")
+        #self.dropout_keep_prob = tf.placeholder(tf.float32, name="dropout_keep_prob")
+
+        # Keeping track of l2 regularization loss (optional)
+        l2_loss = tf.constant(0.0)
+
+        # Embedding layer
+        #with tf.device('/cpu:0'), tf.name_scope("embedding"):
+            #W = tf.Variable(tf.random_uniform([vocab_size, embedding_size], -1.0, 1.0),name="W")
+        self.train_embed = tf.nn.embedding_lookup_sparse(self.embeddings, self.input_x, None)
+        self.train_embed_shape = tf.shape(self.train_embed)
+        self.train_embed = tf.reshape(self.train_embed, [self.train_embed_shape[0], Config.no_intervals_event, -1])
+        self.embedded_chars_expanded = tf.expand_dims(self.train_embed, -1)
+
+        pooled_outputs = []
+        for i, filter_size in enumerate(Config.filter_sizes):
+            with tf.name_scope("conv-maxpool-%s" % filter_size):
+                # Convolution Layer
+                filter_shape = [filter_size, Config.embedding_dim, 1, Config.num_filters]
+                W = tf.Variable(tf.truncated_normal(filter_shape, stddev=0.1), name="W")
+                b = tf.Variable(tf.constant(0.1, shape=[Config.num_filters]), name="b")
+                conv = tf.nn.conv2d( self.embedded_chars_expanded,
+                                    W,
+                                    strides=[1, 1, 1, 1],
+                                    padding="VALID",
+                                    name="conv")
+                # Apply nonlinearity
+                h = tf.nn.relu(tf.nn.bias_add(conv, b), name="relu")
+                # Maxpooling over the outputs
+                pooled = tf.nn.max_pool(
+                    h,
+                    ksize=[1, Config.no_intervals_event - filter_size + 1, 1, 1],
+                    strides=[1, 1, 1, 1],
+                    padding='VALID',
+                    name="pool")
+                pooled_outputs.append(pooled)
+
+        # Combine all the pooled features
+        num_filters_total = Config.num_filters * len(Config.filter_sizes)
+        self.h_pool = tf.concat(pooled_outputs, 3)
+        self.h_pool_flat = tf.reshape(self.h_pool, [-1, num_filters_total])
+
+        # Add dropout
+        with tf.name_scope("dropout"):
+            self.h_drop = tf.nn.dropout(self.h_pool_flat, Config.dropout_keep_prob)
+
+        # Final (unnormalized) scores and predictions
+        with tf.name_scope("output"):
+            W = tf.get_variable(
+                "W",
+                shape=[num_filters_total, Config.num_classes],
+                initializer=tf.contrib.layers.xavier_initializer())
+            b = tf.Variable(tf.constant(0.1, shape=[Config.num_classes]), name="b")
+            l2_loss += tf.nn.l2_loss(W)
+            l2_loss += tf.nn.l2_loss(b)
+            self.scores = tf.nn.xw_plus_b(self.h_drop, W, b, name="scores")
+            self.predictions = tf.argmax(self.scores, 1, name="predictions")
+
+        # CalculateMean cross-entropy loss
+        with tf.name_scope("loss"):
+            losses = tf.nn.softmax_cross_entropy_with_logits(logits=self.scores, labels=self.input_y)
+            self.loss = tf.reduce_mean(losses) + Config.l2_reg_lambda * l2_loss
+
+        # Accuracy
+        with tf.name_scope("accuracy"):
+            correct_predictions = tf.equal(self.predictions, tf.argmax(self.input_y, 1))
+            self.accuracy = tf.reduce_mean(tf.cast(correct_predictions, "float"), name="accuracy")
+
 
 
 def genDictionaries(event_data, is_train):
@@ -58,7 +141,7 @@ def genWordIdIntervals(time_interval):
 def load_embeddings(filename, wordDict):
     dictionary, word_embeds = pickle.load(open(filename, 'rb'))
 
-    embedding_array = np.zeros((len(wordDict), Config.embedding_size))
+    embedding_array = np.zeros((len(wordDict), Config.embedding_dim))
     knownWords = wordDict.keys()
     foundEmbed = 0
     for i in range(len(embedding_array)):
@@ -73,7 +156,7 @@ def load_embeddings(filename, wordDict):
             foundEmbed += 1
             embedding_array[i] = word_embeds[index]
         else:
-            embedding_array[i] = np.random.rand(Config.embedding_size) * 0.02 - 0.01
+            embedding_array[i] = np.random.rand(Config.embedding_dim) * 0.02 - 0.01
     print "Found embeddings: ", foundEmbed, "/", len(knownWords)
 
     return embedding_array
@@ -84,13 +167,20 @@ train_data = open("train_data.txt", "r")
 #non_fake_file = open("non_fake_used.txt", "a")
 wordDict = {}
 total_data = []
-twitter_full_data = train_data.readlines()
+total_dev_data = []
+train_full_data = train_data.readlines()
 train_events = []
-for i in xrange(len(twitter_full_data)):
-    items = twitter_full_data[i].split()
+train_labels = []
+dev_labels = []
+for i in xrange(len(train_full_data)):
+    items = train_full_data[i].split()
     eventid = items[0].split(':')
     labelid = items[1].split(':')
     train_events.append(str(eventid[1]) + "_" + str(labelid[1]) + ".txt")
+    if labelid[1] == "0":
+        train_labels.append([0.0, 1.0])
+    else:
+        train_labels.append([1.0, 0.0])
 
 dev_data = open("dev_data.txt", "r")
 dev_events = []
@@ -100,6 +190,10 @@ for i in xrange(len(dev_full_data)):
     eventid = items[0].split(':')
     labelid = items[1].split(':')
     dev_events.append(str(eventid[1]) + "_" + str(labelid[1]) + ".txt")
+    if labelid[1] == "0":
+        dev_labels.append([0.0, 1.0])
+    else:
+        dev_labels.append([1.0, 0.0])
 
 test_data = open("test_data.txt", "r")
 test_events = []
@@ -110,73 +204,74 @@ for i in xrange(len(test_full_data)):
     labelid = items[1].split(':')
     test_events.append(str(eventid[1]) + "_" + str(labelid[1]) + ".txt")
 
-def get_event_sents(event):
-    json_post_dir = dir+"twitterdata/"
-    full_dir = json_post_dir + event
-    print full_dir
-    data = json.load(open(full_dir))
-    #data = json.load(open("./rumdect/twitterdata/E180_1.txt"))
+def get_event_sents(eventIds):
+    for event1 in eventIds:
+        json_post_dir = dir+"twitterdata/"
+        full_dir = json_post_dir + event1
+        print full_dir
+        data = json.load(open(full_dir))
+        #data = json.load(open("./rumdect/twitterdata/E180_1.txt"))
 
-    pprint(data[0]['created_at'])
-    parsed_date = parser.parse(data[0]['created_at'])
-    timestamp = calendar.timegm(parsed_date.timetuple())
+        pprint(data[0]['created_at'])
+        parsed_date = parser.parse(data[0]['created_at'])
+        timestamp = calendar.timegm(parsed_date.timetuple())
 
-    data = sorted(data, key=lambda dct: calendar.timegm(parser.parse(dct['created_at']).timetuple()))
-    for item in data:
-        item['created_at'] = calendar.timegm(parser.parse(item['created_at']).timetuple())
+        data = sorted(data, key=lambda dct: calendar.timegm(parser.parse(dct['created_at']).timetuple()))
+        for item in data:
+            item['created_at'] = calendar.timegm(parser.parse(item['created_at']).timetuple())
 
-    temp = data[0]['created_at']
-    sum = 0
-    for item in data:
-        item['created_at'] = item['created_at'] - temp
-        sum = sum + item['created_at']
+        temp = data[0]['created_at']
+        sum = 0
+        for item in data:
+            item['created_at'] = item['created_at'] - temp
+            sum = sum + item['created_at']
 
-    for item in data:
-        item['t'] = float(item['created_at'])/sum
+        for item in data:
+            item['t'] = float(item['created_at'])/sum
 
-    #temp = data[len(data) - 1]['t']
-    print (data[len(data) - 1]['created_at'])/20
+        #temp = data[len(data) - 1]['t']
+        print (data[len(data) - 1]['created_at'])/20
 
-    j = 0
-    time_intervals = []
-    interval = []
-    current_index_float = 0.0
-    current_index = 0
-    genDictionaries(data, False)
-    wordid_tweets = genWordIdIntervals(data)
-    div_val = len(wordid_tweets) / 20
-    div_val_float = float(len(wordid_tweets)) / 20
-    div_val_float = div_val_float - div_val
-    print(div_val_float)
-    for i in xrange(len(wordid_tweets)):
-        if current_index >= div_val:
-            current_index_float = current_index_float + div_val_float
-            if current_index_float >= 1.0:
-                current_index_float = current_index_float - 1.0
-                interval.extend(collections.Counter(wordid_tweets[i]))
-                #interval.append(data[i]['text'])
-                current_index = 0
-                time_intervals.append(interval)
-                interval = []
-                continue
-            else:
-                current_index = 0
-                time_intervals.append(interval)
-                interval = []
-        interval.extend(collections.Counter(wordid_tweets[i]))
-        current_index = current_index + 1
-
-    if (current_index_float + 0.000000001) > 1.0:
-        time_intervals[len(time_intervals) - 1].extend(collections.Counter(interval))
+        j = 0
+        time_intervals = []
         interval = []
-    if len(interval) > 0:
-        time_intervals.append(interval)
+        current_index_float = 0.0
+        current_index = 0
+        genDictionaries(data, False)
+        wordid_tweets = genWordIdIntervals(data)
+        div_val = len(wordid_tweets) / 20
+        div_val_float = float(len(wordid_tweets)) / 20
+        div_val_float = div_val_float - div_val
+        print(div_val_float)
+        for i in xrange(len(wordid_tweets)):
+            if current_index >= div_val:
+                current_index_float = current_index_float + div_val_float
+                if current_index_float >= 1.0:
+                    current_index_float = current_index_float - 1.0
+                    interval.extend(collections.Counter(wordid_tweets[i]))
+                    #interval.append(data[i]['text'])
+                    current_index = 0
+                    time_intervals.append(interval)
+                    interval = []
+                    continue
+                else:
+                    current_index = 0
+                    time_intervals.append(interval)
+                    interval = []
+            interval.extend(collections.Counter(wordid_tweets[i]))
+            current_index = current_index + 1
 
-    print "Total Number of intervals",len(time_intervals)
-    #total_data.append(time_intervals)
-    if len(time_intervals) != 20:
-        print "Error"
-    return time_intervals
+        if (current_index_float + 0.000000001) > 1.0:
+            time_intervals[len(time_intervals) - 1].extend(collections.Counter(interval))
+            interval = []
+        if len(interval) > 0:
+            time_intervals.append(interval)
+
+        print "Total Number of intervals",len(time_intervals)
+        total_dev_data.append(time_intervals)
+        if len(time_intervals) != 20:
+            print "Error"
+    return total_dev_data
 
 
 def get_train_sents(eventIds):
@@ -226,11 +321,13 @@ def get_train_sents(eventIds):
                     interval.extend(collections.Counter(wordid_tweets[i]))
                     #interval.append(data[i]['text'])
                     current_index = 0
+                    #interval = np.array(interval)
                     time_intervals.append(interval)
                     interval = []
                     continue
                 else:
                     current_index = 0
+                    #interval = np.array(interval)
                     time_intervals.append(interval)
                     interval = []
             interval.extend(collections.Counter(wordid_tweets[i]))
@@ -240,21 +337,107 @@ def get_train_sents(eventIds):
             time_intervals[len(time_intervals) - 1].extend(collections.Counter(interval))
             interval = []
         if len(interval) > 0:
+            #interval = np.array(interval)
             time_intervals.append(interval)
 
         print "Total Number of intervals",len(time_intervals)
+        #time_intervals = np.array(time_intervals)
         total_data.append(time_intervals)
         if len(time_intervals) != 20:
             print "Error"
             break
+
+    #total_data = np.array(total_data)
     return total_data
 
 
 train_sents = get_train_sents(train_events)
-#test_sents = get_event_sents(test_events[0])
+dev_sents = get_event_sents(dev_events)
 embedding_filename = 'word2vec.model'
 
 embedding_array = load_embeddings(embedding_filename, wordDict)
 print(embedding_array)
+
+print "Generating Traning Examples"
+trainFeats, trainLabels =train_sents, train_labels
+print "Done."
+
+# Build the graph model
+# Training
+# ==================================================
+print("start training...")
+with tf.Graph().as_default():
+    session_conf = tf.ConfigProto(
+        allow_soft_placement=Config.allow_soft_placement,
+        log_device_placement=Config.log_device_placement)
+    sess = tf.Session(config=session_conf)
+    with sess.as_default():
+        cnn = TextCNN(embedding_array)
+
+        # Define Training procedure
+        global_step = tf.Variable(0, name="global_step", trainable=False)
+        optimizer = tf.train.AdamOptimizer(1e-3)
+        grads_and_vars = optimizer.compute_gradients(cnn.loss)
+        train_op = optimizer.apply_gradients(grads_and_vars, global_step=global_step)
+
+        # Keep track of gradient values and sparsity (optional)
+
+        # Initialize all variables
+        sess.run(tf.initialize_all_variables())
+
+
+        def train_step(x_batch, y_batch):
+            """
+            A single training step
+            """
+            feed_dict = {
+                cnn.input_x: tf.SparseTensorValue(),
+                cnn.input_y: y_batch
+            }
+            _, step, loss, accuracy = sess.run([train_op, global_step, cnn.loss, cnn.accuracy],feed_dict)
+
+
+        def dev_step(x_batch, y_batch, writer=None):
+            """
+            Evaluates model on a dev set
+            """
+
+            fo = open("result.txt", "w")
+            feed_dict = {
+                cnn.input_x: x_batch,
+                cnn.input_y: y_batch
+            }
+            step, loss, accuracy, scores = sess.run([global_step, cnn.loss, cnn.accuracy, cnn.scores],feed_dict)
+            y_label = []
+            for i in y_batch:
+                for x in xrange(2):
+                    if (i[x] == 1):
+                        y_label.append(x)
+
+            for ind in xrange(len(scores)):
+                fo.write(str(scores[ind]) + "," + str(y_label[ind]) + "\n")
+            fo.write("\n")
+            time_str = datetime.datetime.now().isoformat()
+            print("eval{}: step {}, loss {:g}, acc {:g}".format(time_str, step, loss, accuracy))
+            #write_json(scores)
+
+
+
+        # Training loop. For each batch...
+        for step in range(Config.num_epochs):
+            start = (step * Config.batch_size) % len(trainFeats)
+            end = ((step + 1) * Config.batch_size) % len(trainFeats)
+            if end < start:
+                start -= end
+                end = len(trainFeats)
+            batch_inputs, batch_labels = trainFeats[start:end], trainLabels[start:end]
+            train_step(batch_inputs, batch_labels)
+            current_step = tf.train.global_step(sess, global_step)
+
+            if current_step % Config.evaluate_every == 0:
+                # print("\nEvaluation:")
+                dev_step(dev_sents, dev_labels)
+            if (current_step == Config.stop_step):
+                break
 
 
